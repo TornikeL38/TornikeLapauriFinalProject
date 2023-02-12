f29b6536425c9fad09a63d47233756ce7c44edc0
TornikeL38 <120584873+TornikeL38@users.noreply.github.com>
2023-02-12 21:20:47 +0400

    Finish registration form

---
+++ app.js
@@ -0,0 +1,22 @@
+// const validateEmail = (email) => {
+//     return String(email)
+//       .toLowerCase()
+//       .match(
+//         /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+//       );
+//   };
+
+//   function onSignup() {
+//     console.log("shemovida")
+//     const email = document.getElementById("emailLabel");
+//     if(validateEmail(email.value)){
+
+//     } else {
+
+//         const lightBox = document.getElementById('lightBox');
+//         console.log(lightBox)
+//         lightBox.id =""
+//         lightBox.classList.add("darkBox");
+
+//     }
+// }
