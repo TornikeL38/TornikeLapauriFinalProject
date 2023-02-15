5e49429af28e5cd086faf431db0d46830a86f38a
TornikeL38 <120584873+TornikeL38@users.noreply.github.com>
2023-02-15 15:48:49 +0400

    js

--- app.js
+++ app.js
@@ -1,22 +1,26 @@
-// const validateEmail = (email) => {
-//     return String(email)
-//       .toLowerCase()
-//       .match(
-//         /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-//       );
-//   };
+const validateEmail = (email) => {
+    return String(email)
+      .toLowerCase()
+      .match(
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      );
+  };
 
-//   function onSignup() {
-//     console.log("shemovida")
-//     const email = document.getElementById("emailLabel");
-//     if(validateEmail(email.value)){
+  function onSignup() {
+    console.log("shemovida")
+    const email = document.getElementById("emailInput");
+    console.log(email.value)
+    if(validateEmail(email.value)){
+        console.log("sworia")
+        const successText = document.getElementById("success");
+        successText.style.visibility = "visible";
 
-//     } else {
+    } else {
 
-//         const lightBox = document.getElementById('lightBox');
-//         console.log(lightBox)
-//         lightBox.id =""
-//         lightBox.classList.add("darkBox");
+        const lightBox = document.getElementById('lightBox');
+        console.log(lightBox)
+        lightBox.id =""
+        lightBox.classList.add("darkBox");
 
-//     }
-// }
+    }
+}
---
+++ weather.js
@@ -0,0 +1,26 @@
+const getWeather = async() => {
+const weatherBar = document.getElementsByClassName('weatherBar')
+    try{
+        const lat = 41.901051;
+        const lon = 45.526001;
+        const key = 'bd9634abdf643c5b99fba404518c5b33'
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${key}`)
+        const data = await response.json()
+        if (response.status === 401) {
+            const errorText = document.createElement("p")
+            errorText.textContent = respone.statusText
+            console.log(errorText)
+            weatherBar.appendchild(errorText)
+        }else{
+            console.log(data)
+            const h2 = document.createElement("h2")
+            h2.textContent = data.name 
+            h2.classList.add(qalaqi)
+            weatherBar.appendchild(h2)
+        }
+    }
+    catch(e){
+
+    }
+}
+getWeather()
