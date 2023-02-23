27f2497a24b56404b68154c784ac34bdf036601f
TornikeL38 <120584873+TornikeL38@users.noreply.github.com>
2023-02-24 01:08:55 +0400

    Add weather.js

--- weather.js
+++ weather.js
@@ -1,28 +1,60 @@
-const getWeather = async() => {
-const weatherBar = document.getElementById('weatherBar')
-console.log(weatherBar)
-    try{
-        const lat = 41.901051;
-        const lon = 45.526001;
-        const key = 'bd9634abdf643c5b99fba404518c5b33'
-        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${key}`)
-        const data = await response.json()
+const fetchWeather = async () => {
+    try {
+        const lat = 41.897785;
+        const lon = 45.549912;
+        const key = "bd9634abdf643c5b99fba404518c5b33";
+        const response = await fetch(
+            `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${key}`
+        )
+        const data = await response.json();
+
         if (response.status === 401) {
-            const errorText = document.createElement("p")
-            errorText.textContent = respone.statusText
-            console.log(errorText)
-            weatherBar.appendchild(errorText)
-        }else{
+            handleError(response);
+        } else {
             console.log(data)
-            const h2 = document.createElement("h2")
-            h2.textContent = data.name 
-            h2.classList.add('city')
 
-            weatherBar.appendChild(h2)
+            let temperatureF = data.main.feels_like;
+            let temperatureC = temperatureF - 273.15;
+            document.getElementById('celsius').textContent = Math.round(temperatureC);
+
+            let locationName = data.name;
+            document.getElementsByClassName('city_name')[0].textContent = locationName +", " + data.sys.country;
+            let weatherIcon = `${data.weather[0].icon}.png`;
+            document.getElementsByClassName('daynight')[0].setAttribute('src',
+                `http://openweathermap.org/img/w/${weatherIcon}`)
+
+            let currentDate = new Date(Date(data.dt));
+            console.log(currentDate)
+            document.getElementsByClassName('date')[0].textContent = currentDate.toDateString();
+
+            let weatherDescription = data.weather[0].description;
+            document.getElementById("description").textContent = weatherDescription;
+
+            let visibility = data.visibility;
+            document.getElementById("visibility").textContent = visibility + "KM";
+
+            let feels_like = data.main.feels_like;
+            document.getElementById("feels_like").textContent = feels_like + "C";
+
+            let humidity = data.main.humidity;
+            document.getElementById("humidity").textContent = humidity + "%";
+
+            let wind = data.wind.speed;
+            document.getElementById("wind").textContent = wind + "KM";
+
+
         }
-    }
-    catch(e){
+    } catch (e) {
 
     }
+
+
 }
-getWeather()
+
+function handleError(response) {
+    const errorText = document.createElement("p");
+    errorText.textContent = response.statusText;
+    weatherContainer.appendChild(errorText);
+}
+
+fetchWeather()
